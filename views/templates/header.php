39f6b8e459b99425fc4502c753a0693daad25da5
Hugo-M-N <Hugomartinnacarino@gmail.com>
2024-06-19 17:23:17 +0200

    Entorno de desarrollo completo

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,1 +1,25 @@
-<header>Header</header>
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <!-- Implementar Inicio de sesión, registro de usuario y verefidicación de administrador -->
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">
+                    EKT
+                </h1>
+            </a>
+
+            <p class="header__texto">Velas Aromaticas Artesanales</p>
+        </div>
+    </div>
+</header>
+<div class="barra">
+    <div class="barra__contenido">
+        <nav class="navegacion">
+            <a href="/" class="navegacion__enlace <?php echo pagina_actual('/') ? 'navegacion__enlace--actual' : '' ?>">blog</a>
+            <a href="/tienda" class="navegacion__enlace <?php echo pagina_actual('/tienda') ? 'navegacion__enlace--actual' : '' ?>">tienda</a>
+        </nav>
+    </div>
+</div>
